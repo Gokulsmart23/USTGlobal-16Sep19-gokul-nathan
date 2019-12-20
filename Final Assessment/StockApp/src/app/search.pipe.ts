961625dfeeb48cd5367ae337bbd4bfb2140de77b
Gokulsmart23 <gokulsmart23@gmail.com>
2019-12-20 20:39:02 +0530

    final assessment

---
+++ Final Assessment/StockApp/src/app/search.pipe.ts
@@ -0,0 +1,18 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'search'
+})
+export class SearchPipe implements PipeTransform {
+
+  transform(product: any[], search: string): any {
+    if (!product) {
+      return null;
+    } else if (!search) {
+      return product;
+    }
+    return product.filter(function(item) {
+      return JSON.stringify(item).toLowerCase().includes(search.toLowerCase());
+    });
+  }
+}
