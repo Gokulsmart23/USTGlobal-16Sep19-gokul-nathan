6f3871578dcf02fea6f9269ef66cecf96d45fa37
Gokulsmart23 <gokulsmart23@gmail.com>
2019-12-20 20:37:53 +0530

    final assessment

--- Final Assessment/StockApp/src/app/app.module.ts
+++ Final Assessment/StockApp/src/app/app.module.ts
@@ -3,32 +3,46 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { AddProductComponent } from './add-product/add-product.component';
-import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { GetAllComponent } from './get-all/get-all.component';
-import { CommonModule } from '@angular/common';
-import { SearchPipe } from './search.pipe';
-import { CartComponent } from './cart/cart.component';
-import { OrderListComponent } from './order-list/order-list.component';
+import { ToastrModule} from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { GetAllProductsComponent } from './get-all-products/get-all-products.component';
+import { SearchProductsComponent } from './search-products/search-products.component';
+import { AddProductsComponent } from './add-products/add-products.component';
+import { ModifyProductsComponent } from './modify-products/modify-products.component';
+import { SearchProductPipe } from './search-product.pipe';
+import { GetFromCartComponent } from './get-from-cart/get-from-cart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
-    AddProductComponent,
-    GetAllComponent,
-    SearchPipe,
-    CartComponent,
-    OrderListComponent
+    HeaderComponent,
+    HomeComponent,
+    AboutComponent,
+    GetAllProductsComponent,
+    SearchProductsComponent,
+    AddProductsComponent,
+    ModifyProductsComponent,
+    SearchProductPipe,
+    GetFromCartComponent
   ],
   imports: [
     BrowserModule,
-    CommonModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    ReactiveFormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot({
+      progressBar: true,
+      timeOut: 2500,
+      positionClass: 'toast-top-center',
+      preventDuplicates: false,
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
